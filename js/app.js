59275b1ceb7653899019fe19b67e2c9ed86e5be4
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2023-09-05 21:53:15 +0700

    shorten

--- js/app.js
+++ js/app.js
@@ -1568,3 +1568,86 @@ async function dpManualBtn(isUpload) {
 }
 
 dpOnLoad()
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const targetBoxes = document.querySelectorAll(".class");
+    const opacityDivs = document.querySelectorAll("m1", "m2", "m3");
+    const changeButton = document.getElementById("changeButton");
+    const additionalDiv = document.getElementById("additionalDiv");
+
+    const classes = ["O6", "O3", "OO", "O9"];
+    const opacityClasses = ["m1", "m2", "m3"];
+    let currentIndex = 0;
+
+    changeButton.addEventListener("click", function () {
+        targetBoxes.forEach((box) => {
+            box.className = classes[currentIndex];
+        });
+        changeButton.innerText = classes[currentIndex];
+
+        opacityDivs.forEach((div, index) => {
+            if (classes[currentIndex] === "OO" && index < 3) {
+                div.style.opacity = 0.4;
+            } else if (classes[currentIndex] === "O3" && index < 2) {
+                div.style.opacity = 0.4;
+            } else if (classes[currentIndex] === "O6" && index < 1) {
+                div.style.opacity = 0.4;
+            } else {
+                div.style.opacity = 1;
+            }
+        });
+        if (classes[currentIndex] === "O9") {
+            additionalDiv.style.backgroundColor = "rgba(37, 37, 37, 1)";
+        } else {
+            additionalDiv.style.backgroundColor = `rgba(37, 37, 37, ${(80 - 20 * currentIndex) / 100})`;
+        }
+
+        currentIndex = (currentIndex + 1) % classes.length;
+    });
+});
+
+
+function toggleSwitch() {
+var switchElement = document.getElementById("switch");
+switchElement.classList.toggle("on");
+
+var contentElement = document.getElementById("gpad-ur");
+contentElement.classList.toggle("joy-ud");
+var contentElement = document.getElementById("gpad-dl");
+contentElement.classList.toggle("joy-ud");
+var contentElement = document.getElementById("gpad-ul");
+contentElement.classList.toggle("joy-rl");
+var contentElement = document.getElementById("gpad-dr");
+contentElement.classList.toggle("joy-rl");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const targetBoxes = document.querySelectorAll(".class");
+    const opacityDivs = document.querySelectorAll(".m1,.m2,.m3");
+    const changeButton1 = document.getElementById("changeButton-1");
+
+
+    const classes = ["O6", "O3", "OO", "O9"];
+    const opacityClasses = ["m1", "m2", "m3"];
+    let currentIndex = 0;
+
+    changeButton1.addEventListener("click", function () {
+        targetBoxes.forEach((box) => {
+            box.className = classes[currentIndex];
+        });
+        opacityDivs.forEach((div, index) => {
+            if (classes[currentIndex] === "OO" && index < 3) {
+                div.style.opacity = 0.4;
+            } else if (classes[currentIndex] === "O3" && index < 2) {
+                div.style.opacity = 0.4;
+            } else if (classes[currentIndex] === "O6" && index < 1) {
+                div.style.opacity = 0.4;
+            } else {
+                div.style.opacity = 1;
+            }
+        });
+        currentIndex = (currentIndex + 1) % classes.length;
+    });
+});
