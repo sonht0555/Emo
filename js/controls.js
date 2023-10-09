9798e9be86ab4c33d05f505e84f74f47912fff29
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2023-10-09 12:34:48 +0700

    Edit notification

--- js/controls.js
+++ js/controls.js
@@ -413,9 +413,14 @@ export default class MgbaControls extends HTMLElement {
 			}
 		}
 		
-
-
-
+		document.addEventListener("visibilitychange", function() {
+			if (document.hidden) {
+				alert("Kabu stopped suddenly because of switching apps.");
+				setTimeout(function() {
+					alert("Audio activation is interrupted.");
+				}, 200);
+			}
+		});
 
 	}
 }
