b99b00d181a1245496c94c6c7025bf38c8ac2720
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2023-10-20 11:45:57 +0700

    new shader

--- js/controls.js
+++ js/controls.js
@@ -427,13 +427,17 @@ export default class MgbaControls extends HTMLElement {
 			applyShaderClass(selectedValue);
 		});
 		function applyShaderClass(selectedValue) {
-			imgshader.classList.remove('sd-1', 'sd-2', 'sd-3');
+			imgshader.classList.remove('sd-1', 'sd-2', 'sd-3', 'sd-4','sd-5' );
 			if (selectedValue === 'option1') {
 				imgshader.classList.add('sd-1');
 			} else if (selectedValue === 'option2') {
 				imgshader.classList.add('sd-2');
 			} else if (selectedValue === 'option3') {
 				imgshader.classList.add('sd-3');
+			} else if (selectedValue === 'option4') {
+				imgshader.classList.add('sd-4');
+			} else if (selectedValue === 'option5') {
+				imgshader.classList.add('sd-5');
 			}
 		}
 		
