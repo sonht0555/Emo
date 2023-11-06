627b29fbf5d3e0b4b1c36cc354ed23ed0046ef29
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2023-11-06 17:02:40 +0700

    New VBA emulator

--- sw.js
+++ sw.js
@@ -1,29 +1,34 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 workbox.setConfig({ debug: false });
-let revision = '22';
+let revision = '1';
 revision = (parseInt(revision) + 1).toString();
 
 workbox.precaching.precacheAndRoute([
   { url: '/', revision: 'revision' },
   { url: '/build/mgba.js', revision: revision },
   { url: '/build/mgba.wasm', revision: revision },
-  { url: '/css/style.css', revision: revision },
+  { url: '/build/vba.js', revision: revision },
+  { url: '/build/vba.wasm', revision: revision },
+  { url: '/css/main.css', revision: revision },
   { url: '/font/04b03b.ttf', revision: revision },
   { url: '/img/favi.png', revision: revision },
   { url: '/img/icon.png', revision: revision },
-  { url: '/js/app.js', revision: revision },
   { url: '/js/controls.js', revision: revision },
   { url: '/js/fileloader.js', revision: revision },
   { url: '/js/game.js', revision: revision },
   { url: '/js/init.js', revision: revision },
+  { url: '/js/localforage.js', revision: revision },
+  { url: '/js/main.js', revision: revision },
   { url: '/js/menu.js', revision: revision },
   { url: '/js/nipplejs.js', revision: revision },
-  { url: '/js/settings.js', revision: revision },
+  { url: '/js/pako.min.js', revision: revision },
   { url: '/js/storage.js', revision: revision },
+  { url: '/js/vba.js', revision: revision },
   { url: '/index.html', revision: revision },
   { url: '/manifest.json', revision: revision },
   { url: '/sw.js', revision: revision },
+  { url: '/vba.html', revision: revision },
 ]);
 
 workbox.routing.registerRoute(
@@ -33,13 +38,11 @@ workbox.routing.registerRoute(
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 20,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 ngày
+        maxAgeSeconds: 7 * 24 * 60 * 60,
       }),
     ],
   })
 );
-
-// Đăng ký chiến lược StaleWhileRevalidate cho các tệp JS và CSS
 workbox.routing.registerRoute(
   /\.(?:js|css|html)$/,
   new workbox.strategies.StaleWhileRevalidate({
